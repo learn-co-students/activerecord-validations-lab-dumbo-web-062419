fc930cd486408194b5c6fa41c485a90354d269da
lgm527 <laurellmccaffrey@gmail.com>
2019-07-23 16:34:20 -0400

    validations done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+
+  validate :clickbait
+
+  CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+
+  def clickbait
+    unless CLICKBAIT.any? { |p| p.match(title) }
+      errors.add(:title, "Title must be clickbait")
+    end
+  end
+
 end
